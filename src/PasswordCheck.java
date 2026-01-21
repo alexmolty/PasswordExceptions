964102c720f67826aa2d1d128a6e22d7e3dc32b0
alexmolty <alexmolt69@gmail.com>
2026-01-22 01:27:09 +0200

    Refactor password validation logic and add null check

--- src/PasswordCheck.java
+++ src/PasswordCheck.java
@@ -8,26 +8,29 @@ public class PasswordCheck {
     public final static String NO_DIGIT_MESSAGE = "no digit";
     public final static String NO_SPEC_SYMBOL_MESSAGE = "no symbol";
     public final static String WRONG_SYMBOL_MESSAGE = "wrong symbol";
+    public final static String NULL = "null";
 
     public static void check(String password) throws IllegalPasswordException {
         List<String> errors = new ArrayList<>();
-        if (password.length() < 8) {
-            throw new IllegalPasswordException(new String[]{WRONG_LENGTH_MESSAGE});
-        }
-        if (password.matches(".*\\s.*")) {
-            throw new IllegalPasswordException(new String[]{WRONG_SYMBOL_MESSAGE});
-        }
-        if (!password.matches(".*[A-Z].*")) {
-            errors.add(NO_UPPER_CASE_MESSAGE);
-        }
-        if (!password.matches(".*[a-z].*")) {
-            errors.add(NO_LOWER_CASE_MESSAGE);
-        }
-        if (!password.matches(".*\\d.*")) {
-            errors.add(NO_DIGIT_MESSAGE);
-        }
-        if (!password.matches(".*[^\\s\\d\\p{L}].*")) {
-            errors.add(NO_SPEC_SYMBOL_MESSAGE);
+        if(password == null) {
+            throw new IllegalArgumentException(NULL);
+        } else if (password.length() < 8) {
+            errors.add(WRONG_LENGTH_MESSAGE);
+        } else if (password.matches(".*\\s.*")) {
+            errors.add(WRONG_SYMBOL_MESSAGE);
+        } else {
+            if (!password.matches(".*[A-Z].*")) {
+                errors.add(NO_UPPER_CASE_MESSAGE);
+            }
+            if (!password.matches(".*[a-z].*")) {
+                errors.add(NO_LOWER_CASE_MESSAGE);
+            }
+            if (!password.matches(".*\\d.*")) {
+                errors.add(NO_DIGIT_MESSAGE);
+            }
+            if (!password.matches(".*[^\\s\\d\\p{L}].*")) {
+                errors.add(NO_SPEC_SYMBOL_MESSAGE);
+            }
         }
         if (!errors.isEmpty()) {
             throw new IllegalPasswordException(errors.toArray(new String[0]));
